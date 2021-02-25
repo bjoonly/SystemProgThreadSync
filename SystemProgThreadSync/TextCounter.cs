8882c1233168cb7a65be51dce18bcb020e73276b
bjoonly <elumnaya@gmail.com>
2021-02-25 21:49:28 +0200

    Init

---
+++ SystemProgThreadSync/TextCounter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace SystemProgThreadSync
+{
+    class TextCounter:INotifyPropertyChanged
+    {
+        int countWords=0;
+        int countLines=0;
+        int countPunctuationMarks=0;
+        public int CountWords { get { return countWords; } set { countWords = value; OnPropertyChanged(); } }
+        public int CountLines { get { return countLines; } set { countLines = value; OnPropertyChanged(); } }
+        public int CountPunctuationMarks { get { return countPunctuationMarks; } set { countPunctuationMarks = value;OnPropertyChanged(); } }
+        public void Calculate(object obj)
+        {
+            lock (this)
+            {
+                FileStream file = (FileStream)obj;
+                StreamReader streamReader = new StreamReader(file);
+                string line;
+                string[] punct = new string[] { ",",".", ";", ":", "-", "—", "!", "?", "\"", "'", "«", "»", "(", ")", "{", "}", "[", "]", "<", ">", "/" };
+                while (!streamReader.EndOfStream)
+                {
+                    line = streamReader.ReadLine();
+
+                        int index = line.IndexOf("...");
+                     while (index != -1)
+                    {
+                        line = line.Remove(index, 3);
+                        index = line.IndexOf("...", index);
+                        CountPunctuationMarks++;
+                    }
+                    foreach (var item in line)
+                    {
+                        if (punct.Contains(item.ToString()))
+                            CountPunctuationMarks++;
+                    }
+                     
+                CountLines++;
+                CountWords += (line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)).Count();
+                    Thread.Sleep(500);
+            }
+        }
+    }
+      
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ SystemProgThreadSync/ViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace SystemProgThreadSync
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+            startCommand = new DelegateCommand(Start, () => !String.IsNullOrEmpty(DirectoryPath));
+            selectFolderCommand = new DelegateCommand(SelectFolderSource);
+            PropertyChanged += (sender, args) =>
+            {
+
+
+                if (args.PropertyName == nameof(DirectoryPath))
+                {
+                    startCommand.RaiseCanExecuteChanged();
+                }
+            };
+            }
+       private string directoryPath;
+        public string DirectoryPath { get { return directoryPath; } set { if (value != directoryPath) { directoryPath = value;OnPropertyChanged(); } } }
+        private TextCounter tC; 
+        public TextCounter TC { get { return tC; } set { if (value != tC) { tC = value;OnPropertyChanged(); } } }
+
+        private readonly ICollection<string> files = new ObservableCollection<string>();
+        public IEnumerable<string> Files => files;
+     
+        public void Start()
+        {
+            try
+            {
+                TC = new TextCounter();
+                var fileNames = Directory.GetFiles(DirectoryPath, "*.txt");
+                files.Clear();
+                foreach (var item in fileNames)
+                {
+                    files.Add(item);
+                }
+                foreach (var item in fileNames)
+                {
+                    FileStream fs = File.OpenRead(item);
+                    ThreadPool.QueueUserWorkItem(TC.Calculate, fs);
+
+                }
+                
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private Command selectFolderCommand;
+        public ICommand SelectFolderCommand => selectFolderCommand;
+        private Command startCommand;
+        public ICommand StartCommand => startCommand;
+        private void SelectFolderSource()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryPath = fbd.SelectedPath;
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
